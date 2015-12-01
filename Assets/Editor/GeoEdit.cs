c9ec45c440db1f55a91f96137dffc9404b22c930
Matthew <matthew.t.kides@cox.net>
2015-11-30 21:07:39 -0700

    Multiple Vertex Selection
    Multiple vertices can now be selected and transformed. Some commenting
    and window "destructor" cleanup.

--- Assets/Editor/GeoEdit.cs
+++ Assets/Editor/GeoEdit.cs
@@ -21,21 +21,21 @@ using System.Collections.Generic;
 
 public class GeoEditWindow : EditorWindow
 {
-    bool enableGeoEdit;
-    GameObject vertObjectPrefab = Resources.Load("GeoEdit/Objects/vertObject") as GameObject;
-    List<GameObject> vertObjects = new List<GameObject>();
-    GameObject selectedObject = null;
-    GameObject vertParent = null;
-    GameObject lastObject = null;
-    GameObject selectedVertex = null;
-    Vector3 vertexLast = new Vector3 (0,0,0);
-    MeshFilter objectMesh = null;
-    Vector3[] objectVerts = null;
+    bool enableGeoEdit = false;
+    GameObject vertObjectPrefab = Resources.Load("GeoEdit/Objects/vertObject") as GameObject; //cache the prefab for vertices
+    List<GameObject> vertObjects = new List<GameObject>(); //list of the vertices of a selected object
+    GameObject selectedObject = null; //the currently selected object (not vertex)
+    GameObject vertParent = null; //the parent of the vertex objects.
+    GameObject lastObject = null; //the last object selected (not vertex)
+    List<GameObject> selectedVertices = new List<GameObject>(); //a list of the current selected vertices
+    List<Vector3> vertexLast = new List<Vector3>(); //the last positions of vertices
+    MeshFilter objectMesh = null; //the mesh of the selected object (not vertex)
+    Vector3[] objectVerts = null; //an array of the positions of an object's meshe's vertices (the actual selected mesh)
 
     [MenuItem("Window/GeoEdit")]
     public static void ShowWindow()
     {
-        EditorWindow.GetWindow(typeof(GeoEditWindow));
+        GetWindow(typeof(GeoEditWindow));
     }
 
     void OnGUI()
@@ -49,12 +49,12 @@ public class GeoEditWindow : EditorWindow
         if (enableGeoEdit)
         {
             //if the vertex parent object doenst exist create it.
-            if(!vertParent)
+            if (!vertParent)
                 vertParent = new GameObject("vertParent");
 
             //if there is a object selected in the scene but no selectedObject set it.
             //and set the vertex parent's parent to the selected object.
-            if(!selectedObject && Selection.activeGameObject)
+            if (!selectedObject && Selection.activeGameObject)
             {
                 selectedObject = Selection.activeGameObject;
                 vertParent.transform.parent = selectedObject.transform;
@@ -65,7 +65,7 @@ public class GeoEditWindow : EditorWindow
                 //if there is a object selected in the scene and the last object selected isint the selected object.
                 //this check is so this only happens once. Might be able to do it with a bool if it isin't used later.
                 if (Selection.activeGameObject && lastObject != selectedObject)
-                {   
+                {
                     //get the mesh of the selected object.
                     objectMesh = selectedObject.GetComponent<MeshFilter>();
                     //if the mesh was set properly.
@@ -88,7 +88,7 @@ public class GeoEditWindow : EditorWindow
                 }
                 //if there is no object selected in the scene or the currently selected object in the scene isint the last
                 // object selected clean up everything.
-                else if(!Selection.activeGameObject || lastObject != Selection.activeGameObject)
+                else if (!Selection.activeGameObject || lastObject != Selection.activeGameObject)
                 {
                     //don't clean up if the current object selected is one of the vertex objects of the last selected object.
                     if (!(Selection.activeGameObject
@@ -100,35 +100,41 @@ public class GeoEditWindow : EditorWindow
                 }
             }
 
-            //if there isint a selected vertex we need to set it if there is one selected
-            //also need to reset it if a new vertex is selected.
-            if(!selectedVertex || (selectedVertex && Selection.activeGameObject != selectedVertex))
+            for (int i = 0; i < Selection.gameObjects.Length; ++i)
             {
-                //make sure it is a vertex selected
-                if (Selection.activeGameObject
-                        && Selection.activeGameObject.transform.parent && Selection.activeGameObject.transform.parent
-                        && Selection.activeGameObject.transform.parent == vertParent.transform)
+                //if the current selected vertices doesn't contain one of the scene selected vertices.
+                if (!selectedVertices.Contains(Selection.gameObjects[i]))
                 {
-                    selectedVertex = Selection.activeGameObject;
-                    vertexLast = selectedVertex.transform.position;
+                    //make sure it is an actual vertex selected
+                    if (Selection.gameObjects[i].transform.parent && Selection.activeGameObject.transform.parent == vertParent.transform)
+                    {
+                        //add the vertex and its position to the selected vertices and the last vertices.
+                        selectedVertices.Add(Selection.gameObjects[i]);
+                        vertexLast.Add(selectedVertices[i].transform.position);
+                    }
                 }
             }
-            //if there is a selected vertex
-            else if(selectedVertex)
+
+            //this section is magic as far as i'm concerned.
+            //if there are any selected vertices
+            if (selectedVertices.Count != 0)
             {
-                //if the vertex object has been moved
-                if (selectedVertex.transform.position != vertexLast)
+                for (int i = 0; i < selectedVertices.Count; ++i)
                 {
-                    vertexLast = selectedVertex.transform.position;
-                    //search through the vertices and find the selected one.
-                    for (int i = 0; i < vertObjects.Count; ++i)
+                    //if any of the vertices have been moved
+                    if (selectedVertices[i].transform.position != vertexLast[i])
                     {
-                        if(vertObjects[i].transform.position == vertexLast && objectMesh)
+                        vertexLast[i] = selectedVertices[i].transform.position;
+                        //search through the vertices and find the selected one.
+                        for (int j = 0; j < vertObjects.Count; ++j)
                         {
-                            //convert the selected vertex's new position to the selected object's local space then set it's vertices.
-                            objectVerts[i] = selectedObject.transform.InverseTransformPoint(vertObjects[i].transform.localPosition);
-                            List<Vector3> vertList = new List<Vector3>(objectVerts);
-                            objectMesh.mesh.SetVertices(vertList);
+                            if (vertObjects[j].transform.position == vertexLast[i] && objectMesh)
+                            {
+                                //convert the selected vertex's new position to the selected object's local space then set it's vertices.
+                                objectVerts[j] = selectedObject.transform.InverseTransformPoint(vertObjects[j].transform.localPosition);
+                                List<Vector3> vertList = new List<Vector3>(objectVerts);
+                                objectMesh.mesh.SetVertices(vertList);
+                            }
                         }
                     }
                 }
@@ -136,11 +142,7 @@ public class GeoEditWindow : EditorWindow
         }
         //if geoedit is disabled
         else
-        {
             cleanObjects();
-            if (vertParent)
-                GameObject.DestroyImmediate(vertParent);
-        }
     }
 
     //clean up the scene
@@ -155,20 +157,43 @@ public class GeoEditWindow : EditorWindow
                 vertObjects[i] = null;
             }
         }
-        //reintilize the list
+        if (vertParent)
+            GameObject.DestroyImmediate(vertParent);
+        //clean and reintilize lists
+        vertObjects.Clear();
+        vertObjects = null;
         vertObjects = new List<GameObject>();
+        selectedVertices.Clear();
+        selectedVertices = null;
+        selectedVertices = new List<GameObject>();
+        vertexLast.Clear();
+        vertexLast = null;
+        vertexLast = new List<Vector3>();
         //set selected objects to null
+        selectedObject = null;
+        vertParent = null;
+        lastObject = null;
         objectMesh = null;
         objectVerts = null;
-        selectedVertex = null;
+    }
+
+    //c++ 4lyfe (make sure any lists are actually nulled properly at close)
+    void destructor()
+    {
+        cleanObjects();
+
+        vertObjects = null;
+        selectedVertices = null;
         selectedObject = null;
-        lastObject = null;
+
+        vertObjectPrefab = null;
+        vertParent = null;
     }
 
     //clean everything if the window is closed.
     void OnDestroy()
     {
-        cleanObjects();
+        destructor();
         if (vertParent)
             GameObject.DestroyImmediate(vertParent);
     }
