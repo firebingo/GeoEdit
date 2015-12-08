00894cd0a2d0b9fbf3fc974f82b69b1eb7c14b06
Alex Carnot <hukacikanaka@yahoo.com>
2015-12-08 13:43:16 -0700

    Implemented vertex grouping when selecting and moving a vertex. I then tried to optimize that code using list.findIndex(). I may have also fixed some grammatical errors in comments.

--- Assets/Editor/GeoEdit.cs
+++ Assets/Editor/GeoEdit.cs
@@ -23,7 +23,7 @@ public class GeoEditWindow : EditorWindow
 {
     bool enableGeoEdit = false;
     GameObject vertObjectPrefab = Resources.Load("GeoEdit/Objects/vertObject") as GameObject; //cache the prefab for vertices
-    List<GameObject> vertObjects = new List<GameObject>(); //list of the vertices of a selected object
+    List<GameObject> vertObjects = new List<GameObject>(); // List of the scene vertices objects of a selected object
     GameObject selectedObject = null; //the currently selected object (not vertex)
     GameObject vertParent = null; //the parent of the vertex objects.
     GameObject lastObject = null; //the last object selected (not vertex)
@@ -45,33 +45,35 @@ public class GeoEditWindow : EditorWindow
 
     void Update()
     {
-        //if geoedit is disabled
+        // If geoedit is disabled
         if (enableGeoEdit)
         {
-            //if the vertex parent object doenst exist create it.
+            // If the vertex parent object doesn't exist create it
             if (!vertParent)
                 vertParent = new GameObject("vertParent");
 
-            //if there is a object selected in the scene but no selectedObject set it.
-            //and set the vertex parent's parent to the selected object.
+            // If we haven't a record of a selectedObject, but the scene has one, make note of that
+            // Then, also set the vertex parent's parent to the selected object
             if (!selectedObject && Selection.activeGameObject)
             {
                 selectedObject = Selection.activeGameObject;
                 vertParent.transform.parent = selectedObject.transform;
             }
-            //if there is a selected object.
+
+            // If there is a selected object
             if (selectedObject)
             {
-                //if there is a object selected in the scene and the last object selected isint the selected object.
+                //if there is an object selected in the scene and the last object selected isn't the selected object.
                 //this check is so this only happens once. Might be able to do it with a bool if it isin't used later.
-                if (Selection.activeGameObject && lastObject != selectedObject)
+                if (Selection.activeGameObject && lastObject != selectedObject) // If the object currently selected is new
                 {
                     //get the mesh of the selected object.
                     objectMesh = selectedObject.GetComponent<MeshFilter>();
+
                     //if the mesh was set properly.
                     if (objectMesh)
                     {
-                        //create a array of all the positions of the meshes' vertices.
+                        //create an array of all the positions of the mesh's vertices.
                         objectVerts = objectMesh.mesh.vertices;
 
                         //create all the vertex objects and set their positions to the meshes vertices.
@@ -86,8 +88,8 @@ public class GeoEditWindow : EditorWindow
                     }
                     lastObject = selectedObject;
                 }
-                //if there is no object selected in the scene or the currently selected object in the scene isint the last
-                // object selected clean up everything.
+                //if there is no object selected in the scene or the currently selected object in the scene isn't the last
+                // object selected, clean up everything.
                 else if (!Selection.activeGameObject || lastObject != Selection.activeGameObject)
                 {
                     //don't clean up if the current object selected is one of the vertex objects of the last selected object.
@@ -100,6 +102,7 @@ public class GeoEditWindow : EditorWindow
                 }
             }
 
+            // Go through selected objects
             for (int i = 0; i < Selection.gameObjects.Length; ++i)
             {
                 //if the current selected vertices doesn't contain one of the scene selected vertices.
@@ -108,40 +111,74 @@ public class GeoEditWindow : EditorWindow
                     //make sure it is an actual vertex selected
                     if (Selection.gameObjects[i].transform.parent && Selection.activeGameObject.transform.parent == vertParent.transform)
                     {
-                        //add the vertex and its position to the selected vertices and the last vertices.
-                        selectedVertices.Add(Selection.gameObjects[i]);
-                        vertexLast.Add(selectedVertices[i].transform.position);
+                        for(int j = 0; j < vertObjects.Count; j++)
+                        {
+                            // If this vertex is in the same position as the selected vertex, select it too
+                            if(vertObjects[j].transform.position == Selection.gameObjects[i].transform.position)
+                            {
+                                if (!selectedVertices.Contains(vertObjects[j]))
+                                {
+                                    selectedVertices.Add(vertObjects[j]);
+                                    vertexLast.Add(vertObjects[j].transform.position);
+                                }
+                            }
+                        }
+                        //add the vertex and its position to the selected vertices and the last vertices
+                        //selectedVertices.Add(Selection.gameObjects[i]);
+                        //vertexLast.Add(selectedVertices[i].transform.position);
                     }
                 }
             }
 
-            //this section is magic as far as i'm concerned.
-            //if there are any selected vertices
+            // If there are any selected vertices
             if (selectedVertices.Count != 0)
             {
-                for (int i = 0; i < selectedVertices.Count; ++i)
+                // Search through selectedVertices
+                for (int i = 0; i < selectedVertices.Count; ++i) 
                 {
-                    //if any of the vertices have been moved
+                    // If any of the vertices have been moved
                     if (selectedVertices[i].transform.position != vertexLast[i])
                     {
-                        vertexLast[i] = selectedVertices[i].transform.position;
-                        //search through the vertices and find the selected one.
-                        for (int j = 0; j < vertObjects.Count; ++j)
+                        // Find any and all vertices of the same position as this vertex
+                        Vector3 vertLastTemp = vertexLast[i];
+                        Vector3 selectedVertTemp = selectedVertices[i].transform.position;
+
+                        // Get the indices of all the related vertexLasts
+                        List<int> relatedVertexLast = new List<int>();
+                        for (int j = 0; j < vertexLast.Count; j++)
                         {
-                            if (vertObjects[j].transform.position == vertexLast[i] && objectMesh)
-                            {
-                                //convert the selected vertex's new position to the selected object's local space then set it's vertices.
-                                objectVerts[j] = selectedObject.transform.InverseTransformPoint(vertObjects[j].transform.localPosition);
-                                List<Vector3> vertList = new List<Vector3>(objectVerts);
-                                objectMesh.mesh.SetVertices(vertList);
-                            }
+                            int t = vertexLast.FindIndex(j, delegate (Vector3 vl) { return vl == vertLastTemp; });
+                            if (t == -1) break;
+                            relatedVertexLast.Add(t);
+                            j = t;
+                        }
+
+                        // Get the indices of all the related vertexObjects
+                        List<int> relatedVertexObjects = new List<int>();
+                        for (int j = 0; j < vertObjects.Count; j++)
+                        {
+                            int t = vertObjects.FindIndex(j, delegate (GameObject vo) { return (vo.transform.position == vertLastTemp || vo.transform.position == selectedVertTemp); });
+                            if (t == -1) break;
+                            relatedVertexObjects.Add(t);
+                            j = t;
+                        }
+
+                        // Go through all the indices found, setting everthing to their new positions P.S Both index lists should be the same, if they're not, something went wrong
+                        for (int j = 0; j < relatedVertexLast.Count; j++)
+                        {
+                            selectedVertices[relatedVertexLast[j]].transform.position = selectedVertTemp;                                               // Set the selectedVertex to new position
+                            vertexLast[relatedVertexLast[j]] = selectedVertTemp;                                                                        // Set the vertexLast to new position
+
+                            vertObjects[relatedVertexObjects[j]].transform.position = selectedVertTemp;                                                 // Set the vertObject to new position
+                            objectVerts[relatedVertexObjects[j]] = selectedObject.transform.InverseTransformPoint(selectedVertTemp);   // Grab the object's vertex in world space and convert it into local space
                         }
                     }
                 }
+
+                objectMesh.mesh.vertices = objectVerts; // update the mesh vertices which work in local space
             }
-        }
-        //if geoedit is disabled
-        else
+        }// end if (enableGeoEdit)
+        else  //if geoedit is disabled
             cleanObjects();
     }
 
